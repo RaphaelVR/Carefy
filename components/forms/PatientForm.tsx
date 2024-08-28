90c66070c489775d3afe53b2dee7772a68c5e5f0
RaphaelVR <raphaelventura55@gmail.com>
2024-08-28 12:59:50 +0200

    Types & PatientForm

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -12,6 +12,7 @@ import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
 
 export enum FormFieldType {
     INPUT = "input",
@@ -35,18 +36,22 @@ const PatientForm = () => {
     },
   })
  
-    function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {  
+    const onSubmit = async ({ name, email, phone }: z.infer<typeof UserFormValidation>) => {  
     setIsLoading(true);
+    
     try {
-      // const userData = { name, email, phone };
+      const userData = { name, email, phone };
 
-      // const user = await createUser(userData);
+      const user = await createUser(userData);
 
-      // if(user) router.push(`/patients/${user.$id}/register`);
+      if(user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.error(error)
     }
-  }
+
+    setIsLoading(false);
+  };
+  
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
