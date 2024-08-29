702b1d6d4419799ecfa5609d7fa7d3f5324a2fca
RaphaelVR <raphaelventura55@gmail.com>
2024-08-29 16:05:58 +0200

    Registration Form && Date Picker && Gender Selection

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -17,6 +17,8 @@ import { FormFieldType } from './forms/PatientForm'
 import Image from 'next/image'
 import PhoneInput from 'react-phone-number-input'
 import { E164Number } from "libphonenumber-js/core";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 
 interface CustomProps {
@@ -57,7 +59,7 @@ const CustomFormField = (props: CustomProps) => {
 }
 
 const RenderField = ({ field, props }: {field: any, props: CustomProps }) => {
-  const { fieldType, iconSrc, iconAlt, placeholder } = props;
+  const { fieldType, iconSrc, iconAlt, placeholder, showTimeSelect, dateFormat } = props;
 
   switch (fieldType) {
     case FormFieldType.INPUT:
@@ -96,6 +98,28 @@ const RenderField = ({ field, props }: {field: any, props: CustomProps }) => {
             className="input-phone"
           />
         </FormControl>
+      )
+    case FormFieldType.DATE_PICKER:
+      return (
+        <div className='flex rounded-md border border-dark-500 bg-dark-400'>
+          <Image 
+            src="/assets/icons/calendar.svg"
+            height={24}
+            width={24}
+            alt="calendar"
+            className='ml-2'
+          />
+            <FormControl>
+              <DatePicker 
+                selected={field.value}
+                onChange={(date) => field.onChange(date)}
+                dateFormat={dateFormat ?? 'MM/dd/yyyy'}
+                showTimeSelect={showTimeSelect ?? false}
+                timeInputLabel="Time:"
+                wrapperClassName='date-picker'
+              />
+            </FormControl>
+        </div>
       )
   }
 }
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -15,8 +15,7 @@ import {
 import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
-  try {
-    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
+  try {    
     const newuser = await users.create(
       ID.unique(),
       user.email,
@@ -37,3 +36,13 @@ export const createUser = async (user: CreateUserParams) => {
     console.error("An error occurred while creating a new user:", error);
   }
 };
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+
+    return parseStringify(user);
+  } catch (error: any) {
+    console.error("An error occurred while fetching user details:", error);
+  }
+}
