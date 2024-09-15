bc82d9a812bc560855d9c054681c6d236b530a34
RaphaelVR <raphaelventura55@gmail.com>
2024-09-15 20:55:48 +0200

    OTP security finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PasskeyModal";
 import { Button } from "@/components/ui/button";
 
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams}: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
 
-      {/* {TODO OTP verification} */}
+      {isAdmin && <PassKeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[469px]">
@@ -36,6 +39,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+      
       <Image
               src="/assets/images/onboarding-img.png"
               height={1000}
